5c7d526a949b34d8beed1e81b81eedbab1734730
thieulong <thieulongph@gmail.com>
2024-01-08 02:02:06 +1100

    Done

---
+++ packages/duckiebot_control/src/pid_node.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+import rospy
+from std_msgs.msg import Float32
+from pid_controller import PIDController
+
+class PIDNode:
+    def __init__(self):
+        rospy.init_node('pid_node')
+
+        kp = 0.21
+        ki = 0.01
+        kd = 7.2
+
+        self.controller = PIDController(kp, ki, kd)
+        rospy.set_param("controller_ready", "ready")
+        rospy.Subscriber('error', Float32, self.error_callback)
+        self.control_pub = rospy.Publisher('control_input', Float32, queue_size=1)
+
+    def error_callback(self, error_msg):
+        error = error_msg.data
+        dt = 0.1
+
+        rospy.loginfo("Current error: {}".format(error))
+        control_signal = self.controller.calculate(error, dt)
+        rospy.loginfo("Control signal: {}".format(control_signal))
+        self.control_pub.publish(Float32(control_signal))
+
+if __name__ == '__main__':
+    try:
+        pid_node = PIDNode()
+        rospy.spin()
+    except rospy.ROSInterruptException:
+        pass
+
