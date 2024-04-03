ebf065c7d7e00942d5e3578e243f06e80811180f
Thieu Long <59945736+thieulong@users.noreply.github.com>
2024-04-03 16:49:11 +1100

    Update duckiebot_intersection_navigation.py

--- packages/duckiebot_control/src/duckiebot_intersection_navigation.py
+++ packages/duckiebot_control/src/duckiebot_intersection_navigation.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python3
 
 import rospy
-from duckietown_msgs.msg import AprilTagDetectionArray, Twist2DStamped, BoolStamped, FSMState
+from duckietown_msgs.msg import AprilTagDetectionArray, Twist2DStamped, BoolStamped, FSMState, WheelEncoderStamped
 import random
 class IntersectionNavigation:
     def __init__(self):
         rospy.Subscriber('/duckiebot/apriltag_detector_node/detections', AprilTagDetectionArray, self.tag_callback)
         rospy.Subscriber('/duckiebot/stop_line_filter_node/at_stop_line', BoolStamped, self.stop_line_callback)
         rospy.Subscriber('/duckiebot/fsm_node/mode', FSMState, self.fsm_callback)
+        rospy.Subscriber('/duckiebot/left_wheel_encoder_node/tick', WheelEncoderStamped, self.left_encoder_callback)
+        rospy.Subscriber('/duckiebot/right_wheel_encoder_node/tick', WheelEncoderStamped, self.right_encoder_callback)
 
         self.mode_pub = rospy.Publisher('/duckiebot/fsm_node/mode', FSMState, queue_size=10)
         self.velocity_pub = rospy.Publisher('/duckiebot/car_cmd_switch_node/cmd', Twist2DStamped, queue_size=10)
@@ -35,6 +37,9 @@ class IntersectionNavigation:
         
         self.last_tag_id = None
         self.last_sign = None
+
+        self.left_encoder_list = list()
+        self.right_encoder_list = list()
     
     def intersection_randomize(self):
         rospy.loginfo(f"Duckiebot is at Intersection. Choosing direction randomly")
@@ -64,6 +69,14 @@ class IntersectionNavigation:
         rospy.loginfo(f"Chosen direction: {chosen_direction}")
         return chosen_direction
     
+    def left_encoder_callback(self, msg):
+        self.left_current_encoder = msg.data
+        self.left_encoder_list.append(self.left_current_encoder)
+
+    def right_encoder_callback(self, msg):
+        self.right_current_encoder = msg.data
+        self.right_encoder_list.append(self.right_current_encoder)
+    
     def stop_duckiebot(self, duration):
         rospy.loginfo(f"Duckiebot stopped")
         self.cmd_msg = Twist2DStamped()
@@ -73,46 +86,54 @@ class IntersectionNavigation:
         self.velocity_pub.publish(self.cmd_msg)
         rospy.sleep(duration)
     
-    def move_duckiebot_forward(self, duration):
-        rospy.loginfo(f"Duckiebot going forward")
-        self.cmd_msg = Twist2DStamped()
-        self.cmd_msg.header.stamp = rospy.Time.now()
-        self.cmd_msg.v = 0.35
-        self.cmd_msg.omega = 1.0
-        self.velocity_pub.publish(self.cmd_msg)
-        rospy.sleep(duration)
-        self.stop_duckiebot(duration=0)
-
-    def turn_duckiebot_left(self, duration):
-        rospy.loginfo(f"Duckiebot turning left")
-        self.cmd_msg = Twist2DStamped()
-        self.cmd_msg.header.stamp = rospy.Time.now()
-        self.cmd_msg.v = 0.35
-        self.cmd_msg.omega = 2.5
-        self.velocity_pub.publish(self.cmd_msg)
-        rospy.sleep(duration)
-        self.stop_duckiebot(duration=0)
-
-    def turn_duckiebot_right(self, duration):
-        self.disable_lane_controller()
-        rospy.loginfo(f"Duckiebot turning right")
-        self.cmd_msg = Twist2DStamped()
-        self.cmd_msg.header.stamp = rospy.Time.now()
-        self.cmd_msg.v = 0.35
-        self.cmd_msg.omega = -2.5
-        self.velocity_pub.publish(self.cmd_msg)
-        rospy.sleep(duration)
-        self.stop_duckiebot(duration=0)
-    
     def handle_movement(self, direction):
+        rate = rospy.Rate(10)
+        rate.sleep()
+        self.disable_lane_controller()
         if direction == "forward":
-            self.move_duckiebot_forward(duration=3)
+            rospy.loginfo(f"Duckiebot moving forward")
+            self.max_encodings = 500
+            self.target_encoder = self.left_encoder_list[0]
+            while self.left_current_encoder in range(self.target_encoder-self.max_encodings, self.target_encoder+self.max_encodings):
+                self.cmd_msg = Twist2DStamped()
+                self.cmd_msg.header.stamp = rospy.Time.now()
+                self.cmd_msg.v = 0.3
+                self.cmd_msg.omega = 0.0
+                self.velocity_pub.publish(self.cmd_msg)
+                rate.sleep()
+            
+            self.stop_duckiebot(duration=0)
+            self.encoder_list = list()
+            
         elif direction == "right":
-            self.move_duckiebot_forward(duration=1.2)
-            self.turn_duckiebot_right(duration=0.8)
+            rospy.loginfo(f"Duckiebot turning right")
+            self.max_encodings = 300
+            self.target_encoder = self.right_encoder_list[0]
+            while self.right_current_encoder in range(self.target_encoder-self.max_encodings, self.target_encoder+self.max_encodings):
+                self.cmd_msg = Twist2DStamped()
+                self.cmd_msg.header.stamp = rospy.Time.now()
+                self.cmd_msg.v = 0.3
+                self.cmd_msg.omega = -2.0
+                self.velocity_pub.publish(self.cmd_msg)
+                rate.sleep()
+            
+            self.stop_duckiebot(duration=0)
+            self.encoder_list = list()
+
         elif direction == "left":
-            self.move_duckiebot_forward(duration=1.5)
-            self.turn_duckiebot_left(duration=1.2)
+            rospy.loginfo(f"Duckiebot turning left")
+            self.max_encodings = 500
+            self.target_encoder = self.left_encoder_list[0]
+            while self.left_current_encoder in range(self.target_encoder-self.max_encodings, self.target_encoder+self.max_encodings):
+                self.cmd_msg = Twist2DStamped()
+                self.cmd_msg.header.stamp = rospy.Time.now()
+                self.cmd_msg.v = 0.3
+                self.cmd_msg.omega = 1.0
+                self.velocity_pub.publish(self.cmd_msg)
+                rate.sleep()
+            
+            self.stop_duckiebot(duration=0)
+            self.encoder_list = list()
 
     def fsm_callback(self, msg):
         if msg.state == "LANE_FOLLOWING":
@@ -124,14 +145,14 @@ class IntersectionNavigation:
         mode_msg = FSMState()
         mode_msg.state = "LANE_FOLLOWING"
         self.mode_pub.publish(mode_msg)
-        # rospy.loginfo("Duckiebot switched to autonomous")
+        rospy.loginfo("Duckiebot switched to autonomous")
         self.current_state = "AUTONOMOUS"
 
     def disable_lane_controller(self):
         mode_msg = FSMState()
         mode_msg.state = "NORMAL_JOYSTICK_CONTROL"
         self.mode_pub.publish(mode_msg)
-        # rospy.loginfo("Duckiebot switched to manual")
+        rospy.loginfo("Duckiebot switched to manual")
         self.current_state = "MANUAL"
 
     def tag_callback(self, tag_array):
@@ -148,7 +169,7 @@ class IntersectionNavigation:
         self.stop_line_detected = msg.data
 
     def main_loop(self):
-        rate = rospy.Rate(5)
+        rate = rospy.Rate(10)
         while not rospy.is_shutdown():
             rospy.loginfo(f"Duckiebot state: {self.current_state}")
             rospy.loginfo(f"Stop lane detected: {self.stop_line_detected}")
@@ -165,6 +186,8 @@ class IntersectionNavigation:
                         self.disable_lane_controller()
                         self.stop_duckiebot(duration=1)
 
+                        rate.sleep()
+
                         if self.last_sign == "Stop":
                             direction = self.intersection_randomize()
                         elif self.last_sign == "T-Intersection":
@@ -179,18 +202,23 @@ class IntersectionNavigation:
                         rospy.loginfo(f"Duckiebot direction: {direction}")
 
                         self.handle_movement(direction=direction)
+                        rate.sleep()
                         self.movement_in_progress = False
                         self.last_sign = None
                         self.last_tag_id = None
                         self.enable_lane_controller()
+
+                        rate.sleep()
 
                     elif self.movement_in_progress == True:
                         rospy.loginfo("Movement in progress, skipping stop_line_callback")
                         return
                     
                 elif self.stop_line_detected == False:
+                    rate.sleep()
                     self.enable_lane_controller()
             elif self.current_state == "MANUAL":
+                rate.sleep()
                 self.disable_lane_controller()
 
             rate.sleep()
